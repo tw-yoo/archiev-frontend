ae63e211afe6939fca0dfa294316a7002c10f578
Taewon Yoo <twyoo@autocrypt.io>
2022-11-14 09:17:42 +0900

    graph 보이게 수정

--- src/components/graph/resultGraph.js
+++ src/components/graph/resultGraph.js
@@ -1,5 +1,6 @@
 import "./resultGraph.css"
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import useLagRadar from "./useLagRadar";
 
 
 const data = [
@@ -49,34 +50,30 @@ const data = [
 
 
 const ReesultGraph = (props) => {
+    // useLagRadar();
     return (
         <>
             <h1>
                 Charts
             </h1>
-            <ResponsiveContainer
-            //  width="100%" height="100%"
-             >
-                <LineChart
-                width={500}
-                height={300}
-                data={data}
-                // margin={{
-                //     top: 5,
-                //     right: 30,
-                //     left: 20,
-                //     bottom: 5,
-                // }}
-                >
-                <CartesianGrid strokeDasharray="3 3" />
-                <XAxis dataKey="name" />
-                <YAxis />
-                <Tooltip />
-                <Legend />
-                <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{ r: 8 }} />
-                <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
-                </LineChart>
-            </ResponsiveContainer>
+            <LineChart
+              width={1000}
+              height={500}
+              data={data}
+              margin={{
+                  top: 5,
+                  right: 30,
+                  left: 20,
+                  bottom: 5,
+            }}>
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="name" />
+              <YAxis />
+              <Tooltip />
+              <Legend />
+              <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{ r: 8 }} />
+              <Line type="monotone" dataKey="uv" stroke="#82ca9d" activeDot={{ r: 8 }} />
+            </LineChart>
         </>
     )
 }
